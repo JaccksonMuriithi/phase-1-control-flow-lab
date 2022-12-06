c81c907a96525b3e96adb9589292645ab247752a
JacksonMuriithi <jacksonmuriithi299@mail.com>
2022-12-06 15:48:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber<=400){
+    return "This one is on me!";
+  }else if(someNumber >= 2000 && someNumber<2500){
+    return "I will gladly take your thirty bucks.";
+  }else if(someNumber>2500){
+    return "No can do.";
+  }
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(City){
+  if(City=== "NYC"){
+    return  "Ok, sounds good.";
+  }else{
+    return "No go.";
+  }
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if(tip==="generous"){
+    return "Thank you so much.";
+  }else if(tip!=="not as generous"){
+    return "Bye.";
+  }else{
+    return "Thank you.";
+  }
 }
